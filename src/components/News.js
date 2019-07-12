9f41b2e28ee1074139aae97ce8463ce66dbb39c6
Daniela Petrova <dani@robotev.com>
2019-07-12 12:59:26 +0300

    spam filter in componentWillReceiveProps

--- src/components/News.js
+++ src/components/News.js
@@ -3,26 +3,40 @@ import PropTypes from 'prop-types'
 import { Article } from './Article' // идти в components не нужно, так как мы уже в этой директории
 
 class News extends React.Component {
+  state = {
+    filteredNews: this.props.data,
+  }
+  componentWillReceiveProps(nextProps) {
+    console.log({ nextProps })
+    console.log({ oldProps: this.props })
+    let nextFilteredNews = [...nextProps.data] //deep clone
+    nextFilteredNews.forEach((item, index) => {
+      if (item.bigText.toLowerCase().indexOf('pubg') !== -1) {
+        item.bigText = 'СПАМ'
+      }
+    })
+    this.setState({ filteredNews: nextFilteredNews })
+  }
   renderNews = () => {
-    const { data } = this.props
+    const { filteredNews } = this.state
     let newsTemplate = null
-    if (data.length) {
-      newsTemplate = data.map(function(item) {
+    if (filteredNews.length) {
+      newsTemplate = filteredNews.map(function(item) {
         return <Article key={item.id} data={item} />
       })
     } else {
-      newsTemplate = <p>К сожалению новостей нет</p>
+      newsTemplate = <p>No news yet...</p>
     }
     return newsTemplate
   }
   render() {
-    const { data } = this.props
+    const { filteredNews } = this.state
     return (
       <div className="news">
         {this.renderNews()}
-        {data.length ? (
+        {filteredNews.length ? (
           <strong className={'news__count'}>
-            Всего новостей: {data.length}
+            All news: {filteredNews.length}
           </strong>
         ) : null}
       </div>
