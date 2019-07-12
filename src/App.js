67a491ab4298b9cbd29ba7f5a219e8ecd19a5320
Daniela Petrova <dani@robotev.com>
2019-07-12 12:36:52 +0300

    async operation using fetch in componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react' // подключение библиотеки React
 import { Add } from './components/Add' // ./ = текущая директория,
 import { News } from './components/News'
-import newsData from './data/news' // импорт по дефолту
 import './App.css' // подключение файла стилей
 
 class App extends React.Component {
   state = {
-    news: newsData.data, // в начальное состояние положили значение из переменной
+    news: null,
+    isLoading: false,
   }
   handleAddNews = data => {
     // сначала мы формируем массив, на основе всего того, что уже было в новостях
@@ -15,13 +15,30 @@ class App extends React.Component {
     // затем обновляем новый массив новостей в this.state.news
     this.setState({ news: nextNews })
   }
+  componentDidMount() {
+    this.setState({ isLoading: true })
+    fetch('http://localhost:3000/data/news.json')
+      .then(response => {
+        return response.json()
+      })
+      .then(newsData => {
+        console.log(this) //что так как мы используем стрелочные функции - мы не потеряли this
+        console.log('приехали данные ', newsData)
+        // this.setState({ isLoading: false, news: newsData.data })
+        // добавили задержку в три секунды
+        setTimeout(() => {
+          this.setState({ isLoading: false, news: newsData.data })
+        }, 3000)
+      })
+  }
   render() {
+    const { news, isLoading } = this.state // все необходимое взяли из state
     return (
       <React.Fragment>
         <Add onAddNews={this.handleAddNews} />
-        <h3>Новости</h3>
-        {/* считали новости из this.state */}
-        <News data={this.state.news} />
+        <h3>News</h3>
+        {isLoading && <p>Loading...</p>}
+        {Array.isArray(news) && <News data={news} />}
       </React.Fragment>
     )
   }
